d82af70b61a7f0d57837cefb8f3e3bd9857e79e9
Eli Strauss <straussed@gmail.com>
2017-02-07 11:16:00 -0500

    Added raw data and discriminant function code

--- analysis.R
+++ analysis.R
@@ -1,4 +1,63 @@
-#dummy file for code
-eggSize <- read.csv("https://raw.githubusercontent.com/ReproducibleQM/penguegg/master/NRPE_eggSize.csv")
-head(eggSize)
-plot(data = eggSize, length~year)
+##################################################################
+##                      Penguegg group                          ##
+##      Changes rockhopper penguin in egg size over time        ##
+##                        RQM Course                            ##
+##################################################################
+
+library(dplyr)
+
+#########################import data##############################
+rawEggSize <- read.csv("NRPE_eggSize_raw.csv")
+names(rawEggSize) <- c('Source', 'SpecimenID', 'Location', 'Day',
+                       'Month', 'Year', 'EggOrder', 'Length', 'Breadth', 'Volume', 'Reference')
+##################################################################
+
+
+
+###################Define discriminant function####################
+
+##'Defines discriminant function with arbitrary cutoff
+##'to determine egg-laying order of eggs listed as
+##'unknown in dataset. Discriminant function comes from
+##'Bond et al. 2016: 
+##'D = 0.58*Length + 0.39*Breadth - 57.48
+##'D = 0.73*Length + 0.50*Breadth - 72.39
+
+assignOrder <- function(length, breadth, cutoff){
+  ##discriminant function
+  #d <- length*0.73 + breadth*0.50 - 72.39
+  d <- -length*0.73 - breadth*0.50 + 72.39
+  #d <- length*0.58 + breadth*0.39 - 57.48
+  probA <- 1/(1+exp(-d))
+  
+  ###Discriminate
+  if(probA > cutoff){
+    return('A')
+  }else if(probA < (1-cutoff)){
+    return('B')
+  }else{return('U')}
+}
+##################################################################
+
+
+######################Apply discriminant function#################
+eggSizeAssigned <- rawEggSize
+eggSizeAssigned[eggSizeAssigned$EggOrder == 'U',]$EggOrder <- mapply(FUN = assignOrder, 
+                                                                     filter(eggSizeAssigned, EggOrder == 'U')$Length, 
+                                                                     filter(eggSizeAssigned, EggOrder == 'U')$Breadth, 
+                                                                     0.66)
+##################################################################
+
+
+###############Compare to Bond et al. assignments#################
+##'The discriminant function above should have reproduced
+##'the assignment procedure in Bond et al. The plots below
+##'suggest that it has done so, with the exception of 
+##'ODDBALLS, which we haven't classified yet in this analysis
+eggSizePub <- read.csv('NRPE_eggSize.csv')
+par(mfrow = c(2,1))
+boxplot(eggSizePub$length ~ eggSizePub$predEggOrder, main = 'Bond et al.')
+boxplot(eggSizeAssigned$Length ~ eggSizeAssigned$EggOrder, main = 'Us')
+plot(eggSizePub$predEggOrder, main = 'Bond et al.')
+plot(eggSizeAssigned$EggOrder, main = 'Us')
+##################################################################
