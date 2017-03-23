7405a504bfea830e3c5db80c4a3843f169163299
Eli Strauss <straussed@gmail.com>
2017-03-23 12:49:20 -0400

    AICc model comparison for basic variables

--- analysis.R
+++ analysis.R
@@ -9,6 +9,7 @@ library(ggplot2)
 library(geosphere)
 library(scales)
 library(RColorBrewer)
+library(AICcmodavg)
 
 #########################import data##############################
 rawEggSize <- read.csv("NRPE_eggSize_raw.csv")
@@ -86,7 +87,7 @@ eggSizeAssigned[eggSizeAssigned$EggOrder == 'U',]$EggOrder <- mapply(FUN = assig
                                                                      AccuracyCutoff)
 ##################################################################
 
-########################CLassify Oddballs#########################
+########################Classify Oddballs#########################
 ##CHANGE CUTOFF HERE
 oddballCutoff <- 3.5
 ##
@@ -118,63 +119,97 @@ boxplot(data = eggSizeCleaned[eggSizeCleaned$EggOrder == 'A',], Volume ~ Year, m
 
 boxplot(data = eggSizeCleaned[eggSizeCleaned$EggOrder == 'B',], Volume ~ Month, main = 'B Eggs')
 boxplot(data = eggSizeCleaned[eggSizeCleaned$EggOrder == 'A',], Volume ~ Month, main = 'A Eggs')
+# 
+#########################Volume ~ Month############################
+# eggSizeNoOdd <- eggSizeNoOdd[complete.cases(eggSizeNoOdd),]
+# 
+# summary(lm(Volume ~ Month, data = eggSizeNoOdd))
+# m1 <- lm(Volume ~ Month, data = eggSizeNoOdd)
+# ##without high leverage data point
+# summary(lm(Volume ~ Month, data = eggSizeNoOdd[-936,]))
+# 
+# ##split by Egg Order
+# summary(lm(Volume ~ Month, data = filter(eggSizeNoOdd, EggOrder == 'B'))) ## p = .00105
+# summary(lm(Volume ~ Month, data = filter(eggSizeNoOdd, EggOrder == 'A'))) ##n.s
+###################################################################
+# 
+# 
+#########################Volume ~ Year#############################
+# m2 <- lm(Volume ~ Year, data = eggSizeNoOdd)
+# boxplot(data = eggSizeNoOdd, Volume ~ Year)
+# 
+# ##Split by egg order
+# summary(lm(Volume ~ Year, data = filter(eggSizeNoOdd, EggOrder == 'B'))) ##n.s
+# summary(lm(Volume ~ Year, data = filter(eggSizeNoOdd, EggOrder == 'A'))) ##p = .00699
+###################################################################
+# 
+# 
+#######################Volume ~ Location###########################
+# m3 <- lm(Volume ~ Location, data = eggSizeNoOdd)
+# boxplot(Volume ~ Location, eggSizeNoOdd[eggSizeNoOdd$EggOrder == 'A',], main = 'A')
+# boxplot(Volume ~ Location, eggSizeNoOdd[eggSizeNoOdd$EggOrder == 'B',], main = 'B')
+# summary(lm(Volume ~  Location, data = filter(eggSizeNoOdd, EggOrder == 'B')))
+# summary(lm(Volume ~  Location, data = filter(eggSizeNoOdd, EggOrder == 'A')))
+###################################################################
 ##################################################################
 #eggSizeNoOdd <- filter(eggSizeCleaned, EggOrder != 'ODDBALL')
 eggSizeNoOdd <- filter(eggSizeCleaned, EggOrder %in% c('A', 'B'))
-eggSizeNoOdd <- eggSizeNoOdd[complete.cases(eggSizeNoOdd),]
-m0 <- lm(Volume ~ EggOrder, data = eggSizeNoOdd)
+
+#################Model selection for basic variables###############
+##'SST at different ranges
+##'SSP at different ranges
+##'Volume as response variable
+##'Location
+##'Month
+##'Egg Order seperately
+##'Year
+
+#eggDat.A <- filter(eggSizeNoOdd, EggOrder == 'A')
+#eggDat.B <- filter(eggSizeNoOdd, EggOrder == 'B')
+eggs <- eggSizeNoOdd
+
+###remove missing data
+eggs <- eggs[complete.cases(eggs),]
+eggs$ConvergentZone <- ifelse(eggs$Location == 'Gough', 1, 0)
+
+###Model selection for basic variables - A eggs
+eggsA <- filter(eggs, EggOrder == 'A')
+mb1a <- lm(data = eggsA, Volume ~ 1)
+mb2a <- lm(data = eggsA, Volume ~ Location)
+mb3a <- lm(data = eggsA, Volume ~ Year)
+mb4a <- lm(data = eggsA, Volume ~ Month)
+mb5a <- lm(data = eggsA, Volume ~ ConvergentZone)
+mb6a <- lm(data = eggsA, Volume ~ Month + Year)
+mb7a <- lm(data = eggsA, Volume ~ Month + Location)
+mb8a <- lm(data = eggsA, Volume ~ Month + ConvergentZone)
+mb9a <- lm(data = eggsA, Volume ~ Year + Location)
+mb10a <- lm(data = eggsA, Volume ~ Year + ConvergentZone)
+mb11a <- lm(data = eggsA, Volume ~ Year + ConvergentZone + Month)
+mb12a <- lm(data = eggsA, Volume ~ Year + Location + Month)
+
+aic.a <- aictab(list(mb1a, mb2a, mb3a, mb4a, mb5a, mb6a, mb7a, mb8a, mb9a, mb10a, mb11a, mb12a))
+aic.a
 
 
-########################Volume ~ Month############################
-summary(lm(Volume ~ Month, data = eggSizeNoOdd))
-m1 <- lm(Volume ~ Month, data = eggSizeNoOdd)
-##without high leverage data point
-summary(lm(Volume ~ Month, data = eggSizeNoOdd[-936,]))
 
-##split by Egg Order
-summary(lm(Volume ~ Month, data = filter(eggSizeNoOdd, EggOrder == 'B'))) ## p = .00105
-summary(lm(Volume ~ Month, data = filter(eggSizeNoOdd, EggOrder == 'A'))) ##n.s
-##################################################################
+###Model selection for basic variables - B eggs
+eggsB <- filter(eggs, EggOrder == 'B')
+mb1b <- lm(data = eggsB, Volume ~ 1)
+mb2b <- lm(data = eggsB, Volume ~ Location)
+mb3b <- lm(data = eggsB, Volume ~ Year)
+mb4b <- lm(data = eggsB, Volume ~ Month)
+mb5b <- lm(data = eggsB, Volume ~ ConvergentZone)
+mb6b <- lm(data = eggsB, Volume ~ Month + Year)
+mb7b <- lm(data = eggsB, Volume ~ Month + Location)
+mb8b <- lm(data = eggsB, Volume ~ Month + ConvergentZone)
+mb9b <- lm(data = eggsB, Volume ~ Year + Location)
+mb10b <- lm(data = eggsB, Volume ~ Year + ConvergentZone)
+mb11b <- lm(data = eggsB, Volume ~ Year + ConvergentZone + Month)
+mb12b <- lm(data = eggsB, Volume ~ Year + Location + Month)
 
+aic.b <- aictab(list(mb1b, mb2b, mb3b, mb4b, mb5b, mb6b, mb7b, mb8b, mb9b, mb10b, mb11b, mb12b))
+aic.b
 
-########################Volume ~ Year#############################
-m2 <- lm(Volume ~ Year, data = eggSizeNoOdd)
-boxplot(data = eggSizeNoOdd, Volume ~ Year)
-
-##Split by egg order
-summary(lm(Volume ~ Year, data = filter(eggSizeNoOdd, EggOrder == 'B'))) ##n.s
-summary(lm(Volume ~ Year, data = filter(eggSizeNoOdd, EggOrder == 'A'))) ##p = .00699
-##################################################################
-
-
-######################Volume ~ Location###########################
-m3 <- lm(Volume ~ Location, data = eggSizeNoOdd)
-boxplot(Volume ~ Location, eggSizeNoOdd[eggSizeNoOdd$EggOrder == 'A',], main = 'A')
-boxplot(Volume ~ Location, eggSizeNoOdd[eggSizeNoOdd$EggOrder == 'B',], main = 'B')
-summary(lm(Volume ~  Location, data = filter(eggSizeNoOdd, EggOrder == 'B')))
-summary(lm(Volume ~  Location, data = filter(eggSizeNoOdd, EggOrder == 'A')))
-
-ggplot()
-##################################################################
-
-######################Multiple Regression#########################
-m4 <- lm(Volume ~ EggOrder + Month, data = eggSizeNoOdd)
-m5 <- lm(Volume ~ EggOrder * Month, data = eggSizeNoOdd)
-m6 <- lm(Volume ~ EggOrder + Year, data = eggSizeNoOdd)
-m7 <- lm(Volume ~ EggOrder * Year, data = eggSizeNoOdd)
-m8 <- lm(Volume ~ EggOrder + Location, data = eggSizeNoOdd)
-m9 <- lm(Volume ~ EggOrder * Location, data = eggSizeNoOdd)
-m10 <- lm(Volume ~ EggOrder * Month + Location, data = eggSizeNoOdd)
-m11 <- lm(Volume ~ EggOrder * Year + Location, data = eggSizeNoOdd)
-m12 <- lm(Volume ~ EggOrder + Location + Year, data = eggSizeNoOdd)
-m13 <- lm(Volume ~ EggOrder + Location + Year + Month, data = eggSizeNoOdd)
-m14 <- lm(Volume ~ EggOrder*Month + Location + Year*EggOrder, data = eggSizeNoOdd)
-##################################################################
-
-########################Model comparison##########################
-aic <- AIC(m0, m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14)
-aic[order(aic$AIC),]
-##################################################################
 
 
 ########################Read in environmental data################
@@ -220,7 +255,7 @@ edatS[edatS$Lon > 180,'Lon'] <- edatS[edatS$Lon > 180,'Lon'] - 360
 edatS$distTristan <- distGeo(c(tristanLon, tristanLat), mapply(c, edatS[,c('Lon', 'Lat')]))/1000
 edatS$distTristanBin <- cut(edatS$distTristan, breaks = bins, right = T)
 edatS$distGough <- distGeo(c(goughLon, goughLat), mapply(c, edatS[,c('Lon', 'Lat')]))/1000
-edatS$distGoughBin <- cut(edatS$distGough, breaks = bins, right = T)
+edatS$distGoughBin <- cut(edatS$distGough, breaks = bins, right = T, labels = c('bin1, bin2 '))
 
 colors <- brewer.pal(5, 'Mono')
 colors2 <- brewer.pal(5, 'Blues')
@@ -228,9 +263,9 @@ colors2 <- brewer.pal(5, 'Blues')
 #points(mapply(c, edatP[,c('Lon', 'Lat')]), col = alpha(colors[edatP$distGoughBin], 0.1))
 #text(x = c(tristanLon, goughLon), y = c(tristanLat, goughLat), labels = c('T', 'G'), col = 'white')
 
-ggplot(aes(x = Lon, y = Lat), data = edatP) +
+ggplot(aes(x = Lon, y = Lat), data = filter(edatP) +
   geom_raster(alpha = 0.5, fill = colors2[edatP$distTristanBin], interpolate = T)+
-  geom_raster(aes(x = Lon, y = Lat), fill = colors[edatP$distGoughBin], alpha =0.5, data = edatP)
+  geom_raster(aes(x = Lon, y = Lat), fill = colors2[edatP$distGoughBin], alpha =0.5, data = edatP)
 
 ##'Potential bin categories
 ##'0-250
@@ -240,7 +275,7 @@ ggplot(aes(x = Lon, y = Lat), data = edatP) +
 ##################################################################
 
 
-##########################Aggregate means#########################
+################Aggregate means of environmental data#############
 
 ##Timing of different lifehistory periods - Cuthbert 2013 (penguins natural history and conservation)
 IncubationMonths <- c(7,8,9,10)
@@ -265,8 +300,10 @@ for(row in 1:length(eggSizeNoOdd[,1])){
   if(location == 'T'){
     #eggSizeNoOdd[row,'IncubationMeanSST'] <- mean(filter(edatS, Mon %in% IncubationMonths, Year == year, distTristan <= IncubationDistT)$M)
     #eggSizeNoOdd[row,'IncubationMeanSSP'] <- mean(filter(edatP, Mon %in% IncubationMonths, Year == year, distTristan <= IncubationDistT)$M)
-    eggSizeNoOdd[row,'ForagingMeanSST'] <- mean(filter(edatS, Mon %in% ForagingMonths, Year == year, distTristan <= ForagingDistT)$M)
-    eggSizeNoOdd[row,'ForagingMeanSSP'] <- mean(filter(edatP, Mon %in% ForagingMonths, Year == year, distTristan <= ForagingDistT)$M)
+    eggSizeNoOdd[row,'ForagingMeanSST_lib'] <- mean(filter(edatS, Mon %in% ForagingMonthsLiberal, Year == year, distTristan <= ForagingDistT)$M)
+    eggSizeNoOdd[row,'ForagingMeanSSP_lib'] <- mean(filter(edatP, Mon %in% ForagingMonthsLiberal, Year == year, distTristan <= ForagingDistT)$M)
+    eggSizeNoOdd[row,'ForagingMeanSST_cons'] <- mean(filter(edatS, Mon %in% ForagingMonthsLiberal, Year == year, distTristan <= ForagingDistT)$M)
+    eggSizeNoOdd[row,'ForagingMeanSSP_cons'] <- mean(filter(edatP, Mon %in% ForagingMonthsLiberal, Year == year, distTristan <= ForagingDistT)$M)
   }else{
     #eggSizeNoOdd[row,'IncubationMeanSST'] <- mean(filter(edatS, Mon %in% IncubationMonths, Year == year, distGough <= IncubationDistG)$M)
     #eggSizeNoOdd[row,'IncubationMeanSSP'] <- mean(filter(edatP, Mon %in% IncubationMonths, Year == year, distGough <= IncubationDistG)$M)
@@ -279,13 +316,7 @@ for(row in 1:length(eggSizeNoOdd[,1])){
 ##################################################################
 
 
-########################Models####################################
-##'SST at different ranges
-##'SSP at different ranges
-##'Volume as response variable
-##'Location
-##'Month
-##'Egg Order
-##'Year
 
-m1 <- lm(data = eggSizeNoOdd, Volume ~ Month + Location + EggOrder + Year + )
+
+
+
